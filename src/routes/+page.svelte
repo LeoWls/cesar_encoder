1d8dd94709b2b1091b081fb37f635190916e495d
LeoWls <leowalers59@gmail.com>
2024-06-11 14:44:55 +0200

    update title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,15 +12,15 @@
 </script>
 
 <div class="flex flex-col items-center min-h-screen bg-gray-200 p-20 justify-center">
-<h1 class="pb-10">Cesar Encoder </h1>
+<h1 class="pb-10 text-4xl font-bold">Cesar Encoder </h1>
 
-<h1>Votre Message :</h1>
+<h2>Votre Message :</h2>
 <textarea class="border rounded shadow mb-4 p-2 w-full max-w-lg" bind:value={messageCoder} placeholder="coder ici"></textarea>
 
-<h1>Clé d'encodage :</h1>
+<h2>Clé d'encodage :</h2>
 <input class="border rounded shadow mb-4 p-2 w-full max-w-lg"type="number" bind:value={keyEncode}>
 
-<h1>Votre message encodé :</h1>
+<h2>Votre message encodé :</h2>
 <div class="w-full max-w-lg p-4 border rounded shadow bg-white">{message}</div>
 
 </div>
--- src/routes/decode/+page.svelte
+++ src/routes/decode/+page.svelte
@@ -13,14 +13,14 @@
 </script>
 
 <div class="flex flex-col items-center min-h-screen bg-gray-200 p-20 justify-center">
-  <h1 class="pb-10">Cesar Decoder</h1>
+  <h1 class="pb-10 text-4xl font-bold">Cesar Decoder</h1>
   
   <h1>Votre Message :</h1>
   <textarea class="border rounded shadow mb-4 p-2 w-full max-w-lg" bind:value={coderMessage} placeholder="décoder ici"></textarea>
   
-  <h1>Clé d'encodage :</h1>
+  <h2>Clé d'encodage :</h2>
   <input class="border rounded shadow mb-4 p-2 w-full max-w-lg" type="number" bind:value={keyEncode} placeholder="Shift">
   
-  <h1>Votre message décodé :</h1>
+  <h2>Votre message décodé :</h2>
   <div class="w-full max-w-lg p-4 border rounded shadow bg-white">{messagedecoder}</div>
 </div>
